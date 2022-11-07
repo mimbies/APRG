75cc0dc465c3d46e3a87c62974c9920342a4b88f
mimbies <116007525+mimbies@users.noreply.github.com>
2022-11-07 14:56:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,82 @@
+//Zugriff auf website mit : http://localhost:3000/login
+
+//später: http://localhost:3000/startseite!
+// FEHLT: Sessions, Cookies, Login-Confirmation mit Datenbank, die Datenbank selbst
+//ALL-CAPS MUSS ERSETZT WERDEN!
+//am liebsten bei variablen nicht mit Großbuchstaben arbeiten, damit wir nirgends durcheinander kommen!!
+//VOR Serverstart: npm install express, sqlite3, better-sqlite3, ejs
+
+
+//Initialisierungen
+
+//Express.js
+const express = require("express");
+const app = express();
+
+// idk was das ist
+app.use(express.urlencoded({extended: true}))
+
+//EJS
+app.engine(".ejs", require("ejs").__express);
+app.set("view engine", "ejs");
+
+
+
+
+//Connection zur Datenbank
+//Datenbank noch nicht vorhanden! :D
+//Idee: später evtl. in Post-Requests unten ändern!
+//Name der Datenbank: datenbank
+//Name von Benutzernamen-rows: benutzername
+//Name von Passwörtern-rows: passwort
+const DATABASE = "datenbank.db";
+let db = require("better-sqlite3")(DATABASE);
+
+//Server Starten
+app.listen(3000,function(){
+    console.log("listening on 3000");
+});
+
+
+ // Get-Requests
+ //Gerne umbenennen!
+ //eventuell später das ./sites/xyz.html umbenennen, weil wir mit ejs oder so arbeiten!
+
+ //Startseite
+app.get("/startseite", function(req,res) {
+    res.sendFile(__dirname + "/sites/startseite.html");
+});
+
+ //Registrierung
+ app.get("/registrierung", function(req,res) {
+    res.sendFile(__dirname + "/sites/registrierung.html");
+});
+
+//Login
+app.get("/login", function(req,res) {
+    res.sendFile(__dirname + "/sites/login.html");
+});
+
+//Startseite nach Login
+app.get("/startseite2", function(req,res) {
+    res.sendFile(__dirname + "/sites/startseite2.html");
+});
+
+//About
+app.get("/about", function(req,res) {
+    res.sendFile(__dirname + "/sites/about.html");
+});
+
+//Kontakt
+app.get("/kontakt", function(req,res) {
+    res.sendFile(__dirname + "/sites/kontakt.html");
+});
+
+//Mein Account
+app.get("/mein_account", function(req,res) {
+    res.sendFile(__dirname + "/sites/mein_account.html");
+});
+
 //Spenden 1 (Übersicht)
 //Umbenennen in Spendenübersicht?
 app.get("/spenden_1", function(req,res) {
@@ -37,25 +116,3 @@ app.get("/css", function(req,res) {
 
 //Zugriff auf images
 app.use(express.static(__dirname + "/images"));
-
-
-
-
-//Post-Request, vergleich User-Input mit Datenbank
-/*eventuell weiteres mit registrierung anlegen - falls ein user zb. schon existiert?
-app.post("/login", function(req,res){
-    const benutzername = req.body.benutzername;
-    const passwort = req.body.passwort;
-    const row = db.prepare('select * from datenbank where benutzername = ?;').get(benutzername);
-    //Wenn der eingegebene Benutzername nicht gefunden wird:
-    if (row == undefined){
-        return res.render("fehler",{"benutzername": benutzername});
-    }
-    //Wenn das eingegebene Passwort mit dem aus der Tabelle (und dem Benutzernamen) übereinstimmt:
-    if(passwort == row.passwort){
-        return res.render("ERFOLGREICH",{"benutzername": benutzername});
-    }
-    else{
-        res.render("ERROR",{"benutzername": benutzername});
-        } 
-}) */
